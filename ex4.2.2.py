0c9010551f1d1cc48872a3b9e3b4e1ffbf2928fa
eugeni.sai <literis8@gmail.com>
2022-02-21 21:09:31 +0700

    Выполнено задание 4.2

--- ex4.2.2.py
+++ ex4.2.2.py
@@ -2,8 +2,16 @@
 
 import socket
 
-#  Список хостов
-urls = ['drive.google.com', 'mail.google.com', 'google.com']
+from time import sleep
 
-for host in urls:
-    host
+urls = {"drive.google.com": socket.gethostbyname("drive.google.com"),
+        'mail.google.com': socket.gethostbyname("mail.google.com"), 'google.com': socket.gethostbyname("google.com")}
+i = 0
+while i < 50:  # для бесконечного цикла заменить i < 50 на 1 == 1...
+    i += 1  # ...и закомментировать эту строчку
+    for host in urls:
+        sleep(5)
+        if urls[host] != socket.gethostbyname(host):
+            print("[ERROR] " + host + " IP mismatch: " + urls[host] + " " + socket.gethostbyname(host))
+        urls[host] = socket.gethostbyname(host)
+        print(host + " - " + urls[host])
