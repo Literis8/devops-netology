8e7498ce14435cfd485890994e69d2b43ab6cae5
eugeni.sai <literis8@gmail.com>
2022-04-15 15:02:14 +0700

    homework 5.5 finished

---
+++ homeworks/homework_5.5/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g7dvkj6090cfq1643o"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g3bmr3urqifor39t74"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8p2nvhjau4j0fmlfsb"
+}
